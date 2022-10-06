7b08e9d8f835f25f662177a2da7635ac0982af40
Christopher Smith <57920513+sabwafei@users.noreply.github.com>
2022-10-06 17:53:41 -0400

    final update

---
+++ CEN4031 Project/client/src/components/create.js
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router";
+
+export default function Create() {
+  const [form, setForm] = useState({
+    name: "",
+    date: "",
+    description: "",
+  });
+  const navigate = useNavigate();
+
+  // These methods will update the state properties.
+  function updateForm(value) {
+    return setForm((prev) => {
+      return { ...prev, ...value };
+    });
+  }
+
+  // This function will handle the submission.
+  async function onSubmit(e) {
+    e.preventDefault();
+
+    // When a post request is sent to the create url, we'll add a new record to the database.
+    const newTicket = { ...form };
+
+    await fetch("http://localhost:5000/record/add", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newTicket),
+    }).catch((error) => {
+      window.alert(error);
+      return;
+    });
+
+    setForm({ name: "", date: "", description: "" });
+    navigate("/");
+  }
+
+  // This following section will display the form that takes the input from the user.
+  return (
+    <div>
+      <h3>Create New Ticket</h3>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            className="form-control"
+            id="name"
+            value={form.name}
+            onChange={(e) => updateForm({ name: e.target.value })}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="date">Date</label>
+          <input
+            type="text"
+            className="form-control"
+            id="date"
+            value={form.date}
+            onChange={(e) => updateForm({ date: e.target.value })}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="date">Description</label>
+          <input
+            type="text"
+            className="form-control"
+            id="description"
+            value={form.description}
+            onChange={(e) => updateForm({ description: e.target.value })}
+          />
+        </div>
+
+        <div className="form-group">
+          <input
+            type="submit"
+            value="Create Ticket"
+            className="btn btn-primary"
+          />
+        </div>
+      </form>
+    </div>
+  );
+}
