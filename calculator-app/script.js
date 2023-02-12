b729f1a38be999c7e2efefa5dbbb46a762ea9008
Christopher Smith <57920513+sabwafei@users.noreply.github.com>
2023-02-12 15:51:10 -0500

    Nav logic feature add
    updated nav logic, to show overlay to prevent content interaction while nav mobile menu is active.

--- calculator-app/script.js
+++ calculator-app/script.js
@@ -9,19 +9,35 @@
 (function Calculator() {
   //selectors
   const menuToggle = document.getElementById("nav-toggle");
-  const mobileLinkMenu = document.getElementById("mobile-links");
-  //fuctions
+  const mobileNavMenu = document.getElementById("mobile-links");
+  const content = document.getElementById("content");
+  const navBlocker = document.getElementById("blocker");
+  navBlocker.style.display = "none";
 
   //event listeners
   menuToggle.addEventListener("click", () => {
-    if (menuToggle.getAttribute("data-state") == "open") {
-      menuToggle.setAttribute("data-state", "closed");
-      mobileLinkMenu.style.display = "none";
-      menuToggle.querySelector("*:first-child").src = "/images/hamburger.png";
-    } else if (menuToggle.getAttribute("data-state") == "closed") {
-      menuToggle.setAttribute("data-state", "open");
-      mobileLinkMenu.style.display = "flex";
+    if (menuToggle.getAttribute("data-state") == "closed") {
+      mobileNavMenu.style.display = "flex";
       menuToggle.querySelector("*:first-child").src = "/images/cross.png";
+      menuToggle.setAttribute("data-state", "open");
+      navBlocker.style.display = "block";
+      window.removeEventListener("click", (e) => {
+        if (e.target == content) {
+          e.preventDefault();
+        }
+      });
+    } else if (menuToggle.getAttribute("data-state") == "open") {
+      mobileNavMenu.style.display = "none";
+      menuToggle.querySelector("*:first-child").src = "/images/hamburger.png";
+      menuToggle.setAttribute("data-state", "closed");
+      window.addEventListener("click", (e) => {
+        if (e.tartget == content) {
+          e.preventDefault();
+        }
+      });
+      navBlocker.style.display = "none";
     }
   });
+
+  //functions
 })();
